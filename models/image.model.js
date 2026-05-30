d507162361d6334b50004ee286de7255ce51d621
Yevgeni Boukingolts <yevgeni.b93@gmail.com>
2026-05-30 03:54:33 +0300

    chore: remove dead fs cleanup hooks from Image model, S3 deletion handled in controllers

--- models/image.model.js
+++ models/image.model.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
 
 const imageSchema = new mongoose.Schema({
     filename: { type: String, required: true },
@@ -9,23 +7,6 @@ const imageSchema = new mongoose.Schema({
     width: Number,
     height: Number,
     createdAt: { type: Date, default: Date.now },
-});
-
-function removeFiles(doc) {
-    if (!doc) return;
-    const basePath = path.join(__dirname, '..', 'uploads');
-    try { fs.unlinkSync(path.join(basePath, doc.url)); } catch (err) { /* ignore */ }
-    try { fs.unlinkSync(path.join(basePath, doc.thumbnail)); } catch (err) { /* ignore */ }
-}
-
-imageSchema.post('findOneAndDelete', function(doc) {
-    removeFiles(doc); 
-});
-
-imageSchema.pre('deleteMany', async function(next) {
-    const docs = await this.model.find(this.getFilter());
-    docs.forEach(removeFiles);
-    next();
 });
 
 module.exports = mongoose.model('Image', imageSchema);
