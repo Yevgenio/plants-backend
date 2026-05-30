ff5eeb30770334e7272ec9ed233539fa27a40d83
Yevgeni Boukingolts <yevgeni.b93@gmail.com>
2026-05-31 00:38:22 +0300

    Remove dead file upload code
    Routes, controller, and default image asset from the old local-disk upload
    flow are unused since migrating to MinIO.

--- routes/file.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fileController = require('../controllers/file.controller');
-
-// Route to handle static file requests
-router.get('/:file', fileController.serveFile);
-
-module.exports = router;
